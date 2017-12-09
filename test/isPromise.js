02e4fce3e1f75f4d4f1d0a9c6a72cc3bb55f7f11
Emanuel Tannert <post@etannert.de>
2017-12-09 18:27:37 +0100

    add more tests for isPromise

--- test/isPromise.js
+++ test/isPromise.js
@@ -1,8 +1,10 @@
 import test from 'ava'
 
+import Q from 'q'
+
 import { isPromise } from '../dist/RouteMachine'
 
-test('recognises native ES6 Promises', t => {
+test('recognise native ES6 Promises', t => {
 
   t.plan(4)
 
@@ -17,4 +19,43 @@ test('recognises native ES6 Promises', t => {
 
   const p4 = p1.then(() => {})
   t.true(isPromise(p4))
+})
+
+test('recognise third-party promises', t => {
+
+  t.plan(1)
+
+  const p1 = Q(true)
+  t.true(isPromise(p1))
+})
+
+test('recognise random thenables', t => {
+
+  t.plan(2)
+
+  const t1 = {
+    then () {
+      return false
+    },
+  }
+  t.true(isPromise(t1))
+
+  function t2 () { return 1 + 1 }
+  t2.then = t2
+  t.true(isPromise(t2))
+})
+
+test('reject other stuff', t => {
+
+  t.plan(9)
+
+  t.false(isPromise(1))
+  t.false(isPromise(false))
+  t.false(isPromise(NaN))
+  t.false(isPromise(null))
+  t.false(isPromise(void 0))
+  t.false(isPromise(global))
+  t.false(isPromise([5, 7, 'qq']))
+  t.false(isPromise({ hey: 87 }))
+  t.false(isPromise(() => 0))
 })
